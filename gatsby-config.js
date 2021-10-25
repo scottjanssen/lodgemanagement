587c1af0309299b6c5221b12b3b4740473dada77
Scott <scottmjanssen@gmail.com>
2021-10-24 23:39:11 -0400

    Initialized firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "lodgemanagement",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyAMau1_A1IxUHrryDAwxrUfHPJnYxHA8a0",
+          authDomain: "lodgemanagement-ccb57.firebaseapp.com",
+          projectId: "lodgemanagement-ccb57",
+          storageBucket: "lodgemanagement-ccb57.appspot.com",
+          messagingSenderId: "646307036697",
+          appId: "1:646307036697:web:533d956100d29ea677b488",
+          measurementId: "G-9ZQEMN52VG"
+        }
+      }
+    },
+  ],
 };
